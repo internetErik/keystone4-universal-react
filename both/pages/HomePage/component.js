e7cd54727a9dd1c23deb0ea983316c4ff6e160b2
Erik Christianson <internet.erik@gmail.com>
2018-08-20 14:52:08 -0500

    Adding an example that uses render on client only coponent

--- both/pages/HomePage/component.js
+++ both/pages/HomePage/component.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
 
+import RenderOnClientOnly from '../../blocks/RenderOnClientOnly';
+
 export default class HomePage extends React.Component {
 
   static propTypes = {
@@ -41,7 +43,12 @@ export default class HomePage extends React.Component {
           ]}
         />
         <div className="home-page__content grid-container">
-          Hello World <button onClick={this.openLightbox} className="p10 bd1-s-black">Open Lightbox</button>
+          <p>
+            Hello World <button onClick={this.openLightbox} className="p10 bd1-s-black">Open Lightbox</button>
+          </p>
+          <RenderOnClientOnly>
+            <div>This will only render on the client, and not on the server</div>
+          </RenderOnClientOnly>
         </div>
       </section>
     )
